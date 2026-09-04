5d4f8432ebffb4af465b14249e035907b343a5f4
ssambitdas <ssambit.das@gmail.com>
2026-09-04 23:02:44 +0530

    Backend

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "rg_sambit" {
+  name     = "rg-sambit"
+  location = "centralindia"
+}
+resource "azurerm_storage_account" "storage_sambit" {
+  name                     = "storagesambit"
+  resource_group_name      = azurerm_resource_group.rg_sambit.name
+  location                 = azurerm_resource_group.rg_sambit.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container_sambit" {
+  name                  = "sambit-container"
+  storage_account_id    = azurerm_storage_account.storage_sambit.id
+  container_access_type = "private"
+}
+
