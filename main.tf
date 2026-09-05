f0bd9d70752e3d295f200974c4c639780a2cd407
ssambitdas <ssambit.das@gmail.com>
2026-09-05 12:47:40 +0530

    test

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg_sambit" {
-  name     = "rg-sambit"
+  name     = "rg-sambit1"
   location = "centralindia"
 }
 resource "azurerm_storage_account" "storage_sambit" {
